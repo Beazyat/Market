f2505872f0d7e4c0eeb2163f85d2bcc4006f0f4b
Beazyat <y.beazyat@gmail.com>
2024-03-30 11:37:05 +0330

    Add a new Table to database with name item and migrate to database. For first time on my project we use imagefield in table field. next make some change in setting.py for store picture of items because picture and audio and video not store in database. and add some command to main urls.py file for load puctures.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-
+from item.models import Category, Item
 # Create your views here.
+
+
 def index(request):
-    return render(request, './index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    context = {
+        'items': items,
+        'categories': categories,
+    }
+    return render(request, './index.html', context)
